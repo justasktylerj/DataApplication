ba34d422d45472adb429aca8df1f46cf732c702e
Tyler jarrard <tjar2074@740s-pl-104.local>
2015-10-13 08:41:44 -0600

    everything

---
+++ app/src/main/java/activity/ctec/dataapplication/DataActivity.java
@@ -0,0 +1,84 @@
+package activity.ctec.dataapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.EditText;
+
+public class DataActivity extends AppCompatActivity
+{
+
+    private Button YesButton;
+    private Button NoButton;
+    private TextView right;
+    private TextView wrong;
+    private boolean answer;
+    private EditText question;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_data);
+
+        YesButton = (Button) findViewById(R.id.YesButton);
+        NoButton = (Button) findViewById(R.id.NoButton);
+        right = (TextView) findViewById(R.id.right);
+        wrong = (TextView) findViewById(R.id.wrong);
+        question = (EditText) findViewById(R.id.question);
+        right.setVisibility(View.INVISIBLE);
+        wrong.setVisibility(View.INVISIBLE);
+
+        setupListeners();
+    }
+
+    private void trueYes()
+    {
+        if(answer == true)
+        {
+            right.setVisibility(View.VISIBLE);
+            wrong.setVisibility(View.INVISIBLE);
+        }
+        else
+        {
+            right.setVisibility(View.INVISIBLE);
+            wrong.setVisibility(View.VISIBLE);
+        }
+    }
+
+    private void falseNo()
+    {
+        if(answer == true)
+        {
+            right.setVisibility(View.INVISIBLE);
+            wrong.setVisibility(View.VISIBLE);
+        }
+        else
+        {
+            right.setVisibility(View.VISIBLE);
+            wrong.setVisibility(View.INVISIBLE);
+        }
+    }
+    private void setupListeners()
+    {
+        YesButton.setOnClickListener(new View.OnClickListener()
+        {
+            public void onClick(View buttonClickView)
+            {
+                trueYes();
+            }
+        });
+        NoButton.setOnClickListener(new View.OnClickListener()
+        {
+            public void onClick(View buttonClickView)
+            {
+                falseNo();
+            }
+        });
+    }
+}
+
